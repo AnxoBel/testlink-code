e2b6425c03881b65730b8e86e4c556dcfac26386
Anxo Beltrán <abeltran@orienteed.com>
2020-02-18 09:59:00 +0100

    Added email sending to followers Changed config to request user emails instead of ids

--- lib/issuetrackerintegration/odooxmlrpcInterface.class.php
+++ lib/issuetrackerintegration/odooxmlrpcInterface.class.php
@@ -60,35 +60,94 @@ class odooxmlrpcInterface extends issueTrackerInterface {
         }
     }
 
+    private function getUserInfobyEmail($email) {
+        $res_users_fields = [
+            'id',
+        ];
+        $res_partner_fields = [
+            'id',
+            'name'
+        ];
+        $criteria = [
+            ['email', '=', $email],
+        ];
+
+        $partnerInfo = $this->APIClient->search_read('res.partner', $criteria, $res_partner_fields, 1);
+        $userInfo = $this->APIClient->search_read('res.users', $criteria, $res_users_fields, 1);
+
+        $mergedUserInfo = [
+            'email' => $email,
+            'userId' => $userInfo[0]['id'],
+            'partnerId' => $partnerInfo[0]['id'],
+            'name' => $partnerInfo[0]['name']
+        ];
+
+        return ($mergedUserInfo);
+    }
+
     function addIssue($summary, $description) {
         $data = [
             'description' => $description,
             'name' => $summary,
             'project_id' => (int) $this->cfg->project_id,
-            'user_id' => (int) $this->cfg->assignee_id,
+            'user_id' => (int) $this->getUserInfobyEmail($this->cfg->assignee)['userId'],
             'stage_id' => (int) $this->cfg->stack_id,
             'x_studio_tasktype' => (int) $this->cfg->task_type,
         ];
 
         try {
             $id = $this->APIClient->create('project.task', $data);
-            $this->APIClient->write('project.task', [$id], ['x_studio_reporter' => $this->cfg->reporter_id]);
+            $this->APIClient->write('project.task', [$id], ['x_studio_reporter' => (int) $this->getUserInfobyEmail($this->cfg->reporter)['userId']]);
 
             $tag_ids_array = array_map('intval', explode(",", $this->cfg->tag_ids));
             $this->APIClient->write('project.task', [$id], ['tag_ids' => [[(int) $this->cfg->task_type, false, $tag_ids_array]]]); //Not sure if the first value is x_studio_tasktype
 
             $followers_args = [[(int) $id]];
-            $partner_ids_array = array_map('intval', explode(",", $this->cfg->partner_ids));
 
+            $partner_ids_array = array_map(function($email) {
+                return (int) $this->getUserInfobyEmail($email)['partnerId'];
+            }, explode(",", $this->cfg->followers));
             $followers_kwargs = ["partner_ids" => $partner_ids_array];
 
             $this->APIClient->model_execute_kw('project.task', 'message_subscribe', $followers_args, $followers_kwargs);
+
+            $this->sendEmail($this->cfg->username, $this->cfg->followers, 'New Testlink bug created: ' . $summary, $this->cfg->uriview . $id);
         } catch (Exception $e) {
             tLog($e->getMessage(), 'ERROR');
         }
 
         return $id;
     }
+
+    private function sendEmail($from, $to, $subject, $body) {
+        /* Send mail via Odoo to the followers we have set */
+        $invitation_mail = [
+            'subject' => $subject,
+            'body_html' => $body,
+            'email_from' => $from,
+            'email_to' => $to
+        ];
+
+        $mail_id = $this->APIClient->create('mail.mail', $invitation_mail);
+
+        /*
+         * kw_args cannot be empty, it doesn't work in other way. That's why 
+         * we set an existing variable to its default value. And it always throws
+         * a ResponseFaultException due to some internal Odoo logic. As we weren't
+         * able to identify which of our parameters is causing this issue, we ignore
+         * this exception.
+         */
+        try {
+            $this->APIClient->model_execute_kw('mail.mail', 'send', [[$mail_id]], ["auto_commit" => false]);
+        } catch (Ripoo\Exception\ResponseFaultException $e) {
+            /* We ignore it*/
+        }
+    }
+
+    /*
+     * This should check the accuracy of the provided variables, but for the moment
+     * it won't be done.
+     */
 
     function canCreateViaAPI() {
         return true;
@@ -128,8 +187,8 @@ class odooxmlrpcInterface extends issueTrackerInterface {
                 "\t<password>PASSWORD</password>\n" .
                 "\t<uribase>http://YOURINSTANCE.odoo.com/</uribase>\n" .
                 "\t<database>DATABASE_NAME</database>\n" .
-                "\t<reporter_id>REPORTER_ID</reporter_id>\n" .
-                "\t<assignee_id>ASIGNEE_ID</assignee_id>\n" .
+                "\t<reporter>REPORTER_EMAIL</reporter>\n" .
+                "\t<assignee>ASIGNEE_EMAIL</assignee>\n" .
                 "\t<project_id>PROJECT_ID</project_id>\n" .
                 "\t<task_type>TASK_TYPE</task_type>\n" .
                 "\t<!-- stack_id is refered to the stack in the kanban-style view,\n" .
@@ -138,9 +197,9 @@ class odooxmlrpcInterface extends issueTrackerInterface {
                 "\t<!-- tag_ids is a list of the ids separated by comma\n" .
                 "\t (e.g. <tag_ids>40,42</tag_ids>) -->\n" .
                 "\t<tag_ids>TAG_IDS</tag_ids>\n" .
-                "\t<!-- partner_ids is a list of the ids separated by comma\n" .
-                "\t (e.g. <partner_ids>50,51</partner_ids>) -->\n" .
-                "\t<partner_ids>PARTNER_IDS</partner_ids>\n" .
+                "\t<!-- followers is a list of the people e-mails that want to follow the task Testlink creates separated by comma\n" .
+                "\t (e.g. <followers>alice@example.com,bob@example.com</followers>) -->\n" .
+                "\t<followers>FOLLOWERS_EMAILS</followers>\n" .
                 "</issuetracker>\n";
 
         return $template;
